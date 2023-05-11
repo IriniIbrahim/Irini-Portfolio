6c461b5a1802b4f2b397e02b17a32c26fd74e9f0
IriniIbrahim <iriniibrahim3@gmail.com>
2023-05-11 21:50:37 +0200

    Addingnew

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-"use strict";
+function openTab(evt, tabName) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
 
 function toggleNav() {
 	let aside = document.getElementById('sidebar');
@@ -9,11 +27,3 @@ let button = document.getElementById('navToggleButton');
 button.addEventListener('click', toggleNav);
 
 
-$(document).ready(function () {
-	$("a.scrollLink").click(function (event) {
-		event.preventDefault();
-		$("html, body").animate({
-			scrollTop: $($(this).attr("href")).offset().top
-		}, 500);
-	});
-});
